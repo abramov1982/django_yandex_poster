526e843a2a4c4ea8964d9d4940bce90dbe742af0
abramovio@it2.ru <abramovio@it2g.ru>
2021-11-04 12:53:54 +0300

    Add Place model

--- apps/places/models.py
+++ apps/places/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Place(models.Model):
+    title = models.CharField(max_length=200, blank=False, verbose_name='Название')
+    description_short = models.TextField(blank=False, verbose_name='Краткое описание')
+    description_long = models.TextField(blank=False, verbose_name='Полное описание')
+    coordinates = models.JSONField(blank=False, verbose_name='координаты места')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
+        app_label = 'places'
+        db_table = 'place'
