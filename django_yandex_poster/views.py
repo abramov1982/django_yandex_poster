2c85519e04b60437ecd1211cdb3f4a18aeba2d8f
abramovio@it2.ru <abramovio@it2g.ru>
2021-11-04 13:58:02 +0300

    Move data from template to view

--- django_yandex_poster/views.py
+++ django_yandex_poster/views.py
@@ -1,8 +1,38 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from django.template import loader
 
 
 def index(request):
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    places = {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.62, 55.793676]
+          },
+          "properties": {
+            "title": "«Легенды Москвы",
+            "placeId": "moscow_legends",
+            "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json"
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.64, 55.753676]
+          },
+          "properties": {
+            "title": "Крыши24.рф",
+            "placeId": "roofs24",
+            "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/roofs24.json"
+          }
+        }
+      ]
+    }
+    context = {'places': places}
+    return render(request, 'index.html', context)
