a98f813af317020d5016792b237bcba8548fd89a
abramovio@it2.ru <abramovio@it2g.ru>
2021-12-09 16:29:53 +0300

    Исправлено именование переменной

--- apps/places/management/commands/load_place.py
+++ apps/places/management/commands/load_place.py
@@ -17,32 +17,32 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         repo_url = 'https://api.github.com/repos/devmanorg/where-to-go-places/git/trees/master?recursive=1'
         raw_url = 'https://raw.githubusercontent.com/devmanorg/where-to-go-places/master/'
-        place_files_request = requests.get(repo_url)
-        place_files_request.raise_for_status()
-        serialized_files = place_files_request.json()
+        place_files_response = requests.get(repo_url)
+        place_files_response.raise_for_status()
+        serialized_files = place_files_response.json()
         for file in serialized_files['tree']:
             filename = urlparse(unquote(file['path'])).path
             if not os.path.splitext(filename)[1] == '.json':
                 continue
-            place_raw_request = requests.get(raw_url + file['path'])
-            place_raw_request.raise_for_status()
-            place_serialize = place_raw_request.json()
+            place_raw_response = requests.get(raw_url + file['path'])
+            place_raw_response.raise_for_status()
+            place_serialized = place_raw_response.json()
             place, created = Place.objects.get_or_create(
-                title__iexact=place_serialize['title'],
-                latitude=place_serialize['coordinates']['lat'],
-                longitude=place_serialize['coordinates']['lng'],
-                defaults={'title': place_serialize['title'],
-                          'description_long': place_serialize['description_long'],
-                          'description_short': place_serialize['description_short'],
-                          'latitude': place_serialize['coordinates']['lat'],
-                          'longitude': place_serialize['coordinates']['lng'],
+                title__iexact=place_serialized['title'],
+                latitude=place_serialized['coordinates']['lat'],
+                longitude=place_serialized['coordinates']['lng'],
+                defaults={'title': place_serialized['title'],
+                          'description_long': place_serialized['description_long'],
+                          'description_short': place_serialized['description_short'],
+                          'latitude': place_serialized['coordinates']['lat'],
+                          'longitude': place_serialized['coordinates']['lng'],
                           }
             )
-            print(f'{place_serialize["title"]} was save')
-            for image_url in place_serialize['imgs']:
-                image_request = requests.get(image_url)
-                image_request.raise_for_status()
-                image_data = IMG.open(BytesIO(image_request.content))
+            print(f'{place_serialized["title"]} was save')
+            for image_url in place_serialized['imgs']:
+                image_response = requests.get(image_url)
+                image_response.raise_for_status()
+                image_data = IMG.open(BytesIO(image_response.content))
                 image_name = urlparse(unquote(image_url)).path.split('/')[-1]
                 image_data.save(f'./media/places/images/{image_name}')
                 image, created = Image.objects.get_or_create(place=Place.objects.get(pk=place.pk),
