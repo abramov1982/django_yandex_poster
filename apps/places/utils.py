d86988f36af9dacb6c45b58fe1a1f861c924f24f
abramovio@it2.ru <abramovio@it2g.ru>
2021-12-09 16:16:26 +0300

    Разделил функцию make_geojson на атомарные функции

--- apps/places/utils.py
+++ apps/places/utils.py
@@ -1,19 +1,33 @@
+import collections
+
 from .models import Place
 
 
-def make_geojson():
+def get_places(places_pk):
+    if places_pk:
+        places = Place.objects.filter(pk__in=places_pk)
+    else:
+        places = Place.objects.all()
+    return places
+
+
+def place_detail(place):
+    detail = {"type": "Feature",
+              "geometry": {
+                  "type": "Point",
+                  "coordinates": [place.longitude, place.latitude]
+              },
+              "properties": {
+                  "title": place.title,
+                  "detailsUrl": f'places/{place.pk}'
+              }
+              }
+    return detail
+
+
+def make_geojson(places_pk=None):
     geojson = {"type": "FeatureCollection", "features": []}
-    places = Place.objects.all()
+    places = get_places(places_pk)
     for place in places:
-        place_detail = {"type": "Feature",
-                        "geometry": {
-                          "type": "Point",
-                          "coordinates": [place.longitude, place.latitude]
-                                    },
-                        "properties": {
-                          "title": place.title,
-                          "detailsUrl": f'places/{place.pk}'
-                                      }
-                        }
-        geojson["features"].append(place_detail)
+        geojson["features"].append(place_detail(place))
     return geojson
